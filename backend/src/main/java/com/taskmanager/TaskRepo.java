9bbbaa4fe96c057c3f0946487ff296501d1f0ad9
Austin Dossantos <austin.dossantos019@gmail.com>
2026-02-24 18:25:46 -0500

    Configured Neon and Flyway

--- backend/src/main/java/com/taskmanager/TaskRepo.java
+++ backend/src/main/java/com/taskmanager/TaskRepo.java
@@ -2,6 +2,4 @@ package com.taskmanager;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepo extends JpaRepository<Task, Long> {
-
-}
+public interface TaskRepo extends JpaRepository<Task, Long> {}
--- backend/src/main/java/com/taskmanager/User.java
+++ backend/src/main/java/com/taskmanager/User.java
@@ -1,22 +1,40 @@
 package com.taskmanager;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
 
 @Entity
-@Data
-@Table(name="USERS")
+@Table(name="users")
 public class User {
 
     @Id
+    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long userId;    // User.userId (PK)
     
     private String name;
+
+    @Column(name = "email_address", nullable = false, unique = true)
     private String emailAddress;
+
+    @Column(name = "hashed_password", nullable = false, unique = false)
     private String hashedPassword;
+
+    public User() {}
+
+    public User(String name, String emailAddress, String hashedPassword) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.hashedPassword = hashedPassword;
+    }
+
+    // Getters
+    public Long getUserId() { return userId; }
+    public String getName() { return name; }
+    public String getEmailAddress() { return emailAddress; }
+    public String getHashedPassword() { return hashedPassword; }
+
+    // Setters
+    public void setName(String name) { this.name = name; }
+    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress; }
+    public void setHashedPassword(String hashedPassword) { this.hashedPassword = hashedPassword; }
 }
