b9e1512ff5704a63db08e5ab0547cfe4a1ba3951
Austin Dossantos <austin.dossantos019@gmail.com>
2026-03-13 19:10:00 -0400

    Initial commit for Tasks REST API

---
+++ backend/src/main/java/com/taskmanager/dto/taskdto/UpdateTaskRequest.java
@@ -0,0 +1,33 @@
+package com.taskmanager.dto.taskdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+
+import com.taskmanager.entity.ProjectStatus;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+// Include fields the client chooses when updating a task
+public class UpdateTaskRequest {
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    @NotNull
+    private ProjectStatus status;
+
+    @NotNull
+    private LocalDate dueDate;
+
+    @NotNull
+    private Long assignedTo;
+}
