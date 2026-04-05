0e46228dc00c3065294148687841ecee54cf2299
Austin Dossantos <austin.dossantos019@gmail.com>
2026-04-04 22:15:50 -0400

    Updated createProject API to allow adding members at project creation

--- backend/src/main/java/com/taskmanager/service/impl/ProjectServiceImpl.java
+++ backend/src/main/java/com/taskmanager/service/impl/ProjectServiceImpl.java
@@ -1,7 +1,9 @@
 package com.taskmanager.service.impl;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.taskmanager.dto.projectdto.CreateProjectRequest;
 import com.taskmanager.dto.projectdto.ProjectResponse;
 import com.taskmanager.dto.projectdto.UpdateProjectRequest;
+import com.taskmanager.dto.projectmember.CreateProjectMemberRequest;
 import com.taskmanager.entity.Project;
 import com.taskmanager.entity.ProjectMember;
 import com.taskmanager.entity.ProjectMemberRole;
@@ -46,6 +49,27 @@ public class ProjectServiceImpl implements ProjectService{
         ProjectMember ownerMembership = new ProjectMember(savedProject, user, ProjectMemberRole.OWNER);
         projectMemberRepo.save(ownerMembership);
 
+        // Add optional members to project
+        if(request.getMembers() != null) {
+            Set<String> processedEmails = new HashSet<>();
+
+            for(CreateProjectMemberRequest memberRequest : request.getMembers()) {
+                String email = memberRequest.getEmail().trim().toLowerCase();
+
+                boolean isValidEmail = !email.isEmpty();
+                boolean isNotDuplicate = processedEmails.add(email);
+
+                if(isValidEmail && isNotDuplicate) {
+                    User member = userRepo.findByEmailAddress(email).orElseThrow(
+                        () -> new ResourceNotFoundException("User does not exist for the given email: " + email));
+
+                    // If the member to be added isn't actually the owner, add the member
+                    if(!member.getUserId().equals(user.getUserId())) {
+                        projectMemberRepo.save(new ProjectMember(savedProject, member, memberRequest.getRole()));
+                    }  
+                }
+            }
+        }
         return ProjectMapper.mapToProjectResponse(savedProject);
     }
 
