5587bbad8ff05138fd596ef7ba927b7a8a3e0f47
samtejeda <samueltejedavalencia@gmail.com>
2026-04-24 13:11:46 -0400

    modified frontend URL in CorsConfig.java to deploy frontend and backend

--- backend/src/main/java/com/taskmanager/config/CorsConfig.java
+++ backend/src/main/java/com/taskmanager/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://project-manager-beta-ten.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
