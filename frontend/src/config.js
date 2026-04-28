8532efe3cd0b4eae2e968c462626a92b55c0437f
Austin Dossantos <austin.dossantos019@gmail.com>
2026-04-28 16:41:21 -0400

    Added frontend vercel URL to signup page

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
 export const apiBaseUrl = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+export const apiRealUrl = import.meta.emv.VITE_API_BASE_URL ?? "https://project-manager-beta-ten.vercel.app"
