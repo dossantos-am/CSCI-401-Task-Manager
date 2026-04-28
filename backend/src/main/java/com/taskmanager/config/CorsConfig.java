d6d8b637990af613f074e82ebd56a3eb3c6c87c6
Austin Dossantos <austin.dossantos019@gmail.com>
2026-04-28 19:02:08 -0400

    Fixed bug with call to setAllowedOrigins being called twice

--- backend/src/main/java/com/taskmanager/config/CorsConfig.java
+++ backend/src/main/java/com/taskmanager/config/CorsConfig.java
@@ -14,8 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://csci-401-task-manager.vercel.app"));
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://csci-401-task-manager.vercel.app", "http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
