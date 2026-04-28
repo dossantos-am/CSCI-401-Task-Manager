d6932672135d58b7eb5fb34a5ad4d107c02e02da
Austin Dossantos <austin.dossantos019@gmail.com>
2026-04-28 18:59:13 -0400

    Added https:// to cors allowed origin for vercel url

--- backend/src/main/java/com/taskmanager/config/CorsConfig.java
+++ backend/src/main/java/com/taskmanager/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("csci-401-task-manager.vercel.app"));
+        config.setAllowedOrigins(List.of("https://csci-401-task-manager.vercel.app"));
         config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
