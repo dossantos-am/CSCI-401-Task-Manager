e6823ff1c8a477b813a0f28c7a837069dff32017
Austin Dossantos <austin.dossantos019@gmail.com>
2026-04-30 20:07:19 -0400

    Gave the parent div for Project Info and Task List a max height so TaskList's ability to stretch is limited

--- frontend/src/pages/SingleProject.jsx
+++ frontend/src/pages/SingleProject.jsx
@@ -135,7 +135,7 @@ const SingleProject = () => {
           canEdit={canEdit}
         />
 
-        <div className="space-y-3">
+        <div className="space-y-3 h-full">
           
           {taskLoading ? (
             <p className="text-gray-500">Loading tasks...</p>
